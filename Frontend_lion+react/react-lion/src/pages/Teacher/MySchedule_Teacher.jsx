0b6130fa657f2c6d208077fbf006c132d0d98a90
Alejandra <92651757+aleja1020ob@users.noreply.github.com>
2023-11-26 13:52:32 -0500

    add: paginas "Teacher"

--- Frontend_lion+react/react-lion/src/pages/Teacher/MySchedule_Teacher.jsx
+++ Frontend_lion+react/react-lion/src/pages/Teacher/MySchedule_Teacher.jsx
@@ -5,16 +5,16 @@ import "../../css/Teacher_Schelue.css";
 function MyScheduleT() {
   return (
     <div className="row bodyAdminPanel" id="contenedor-wrap">
-      <SidebarHome className="col-md-5 p-3" />
-      <div className="col-md-7 p-0 mb-0" style={{ marginTop: "-100px" }}>
-        <h1>CURSOS</h1>
-        <section id="table">
-          <div className="imgDiv">
-            <img src={imagesHorario} alt="" className="images" />
-          </div>
-        </section>
-      </div>
+    <SidebarHome className="col-md-5 p-3" />
+    <div className="col-md-7 col-8  responsive-col" >
+      <h1 id="h_schedule">CURSOS</h1>
+      <section id="table">
+        <div className="imgDiv">
+          <img src={imagesHorario} alt="" className="images" />
+        </div>
+      </section>
     </div>
+  </div>
   );
 }
 
