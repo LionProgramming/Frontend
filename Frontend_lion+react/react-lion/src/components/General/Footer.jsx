2add112308a28d8dfe9599cebc45b0709dcfc22b
Alejandra <92651757+aleja1020ob@users.noreply.github.com>
2024-02-05 23:33:21 -0500

    add: funcionalidad home page

--- Frontend_lion+react/react-lion/src/components/General/Footer.jsx
+++ Frontend_lion+react/react-lion/src/components/General/Footer.jsx
@@ -4,19 +4,16 @@ function Footer() {
     <footer id="contacto">
       <div className="contenerdor contenedor_footer">
         <div className="contacto_us">
-          <h2 className="marca">Colegio Agustiniano norte</h2>
+          <h2 className="marca">COLEGIO I.T.D. JUAN DEL CORRAL I.E.D.</h2>
           <p>
-            La unión estudiantil promueve la participación, defiende nuestros derechos y lucha por una educación justa y accesible para todos.
+            L“La formación humana, científica y tecnológica para el desarrollo del ciudadano en el siglo XXI”
           </p>
         </div>
         <div className="redes_sociales">
-          <a href="./" className="red_csocial">
+          <a href="https://twitter.com/juandelcorral10?ref_src=twsrc%5Etfw%7Ctwcamp%5Eembeddedtimeline%7Ctwterm%5Escreen-name%3Ajuandelcorral10%7Ctwcon%5Es2" className="red_csocial">
             <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAUdJREFUSEvdleExBEEQhb+LgAzIABEgAkTgRIAIEAEhyAARkAGXAREgAuq7mrmanZuZ29uq/XNTdX92ut/r97qnb8LIZzIyPptHsA38rLDNmEvgFNgHPoFH4C7k+f05YuQWGWzAR4PkDTgs3Jsj4TXwUCLw8j0oOAMEyk+MqfFLMgsE8yJzBX9JplUoO7VMdU8Ndb/BgUVxOYHenWQAfvOnfQfAfYPgJRAsQlICm7cbwHYGvg8V36a5OcH3QOCY1mlwqQfaMLR68bSwM4F5D5wSG7Q1QMlXsLiTWloVTsp53qwehEv2lCzy2xHw2gMwDbF61S9tgdqycxJu1iA5rjzM5jZ1bH1s2tU6F2EXFWNqCrQpLrQauK/2qgWe90BQK97rYY0vVnDHunlKCqah0TYtkrnAbGC6NlZhz+837x+tl+x1gka36B/CnzgZFPaBJAAAAABJRU5ErkJggg==" alt="" />
           </a>
-          <a href="./" className="red_csocial">
-            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAX1JREFUSEvNlU0yBEEQhb+5gZUtToATYGOLA4jgBm6AEwg7O5zAWFrhBDgB4QJ+LkB8olLUVJTumkXHyIiKmumuypf5MvP1iIFtNLB/ZgawCBwCK2l1JfoIuE7TPnG2lsEecJ5O3QPvtYvpvQHMAWvp/z5wkSOUAF54AHS8nZy3lMmMx8AysJoHVAKIrmMvGHmf7QLHwAewAzwDl4As/FgJ8AK41iueBTZCTc6vgU3gBngFFoC7tC/9BfCVinWQAcjxbaXYOjsDPoH5FLnPrMdv4GUGApjyUQZgtFJm2vKsmY10Wq+N7OzUADq6SvyG8/AX3Sb38W5qADNxHmrtLHVvRcb/D2BwiqIlbUE5tzU1f58AT0VLN1GkppRt6sWYgShybdp7AfoGTSmJrMqu8rkAtrSrOsn29hbgJLZIRT7wOu2VihA7I1EZzajFdO68eL9T7KKAIdcCdcl1aFbsvXId0RqJhfaiHdRlKqmBOJTKyoTN7JPZwnvTmcEz+AZJz2cZU1H38wAAAABJRU5ErkJggg==" alt="" />
-          </a>
-          <a href="./" className="red_csocial">
+          <a href="https://www.facebook.com/juandelcorralied/posts/pfbid02uVbgeweMNkpGFgwKykb7ehaoMJtypMTQ4VjvKG5A3TkXiHxv7TzeDDwT4zpKRMfl?ref=embed_page" className="red_csocial">
             <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAL1JREFUSEvtlIEJwjAQRV836AY6ghtUJ9EROoJOoCO4ghvoBDqCbuAGlQ8JHGo9UhJUaKBw0Ob/+4+7VhQ+VWF9fs5gCmyBGaBa5wTM+0ikJJDgGaifxLIZ7IHlm06zGRyBJhgcgBVw94YkBVFnxDbA2hPX+68bCMMkdGo7Fi49cYpi/RLKS2C59xH5iCuHwcKkSU6wC0uli3GCVN+Aa1BrgUuORfvPKbLJxwTu4o+IXESDPvB+FYNE7aXiBg+0MykZNmDKCAAAAABJRU5ErkJggg==" alt="" />
           </a>
         </div>
--- Frontend_lion+react/react-lion/src/pages/Layout/HomePage.jsx
+++ Frontend_lion+react/react-lion/src/pages/Layout/HomePage.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import "../../css/HomePage.css";
 import Footer from "../../components/General/Footer.jsx";
 import img1 from "../../images/icono.svg";
@@ -13,108 +14,146 @@ import img10 from "../../images/icono1.svg";
 import img11 from "../../images/icono3.svg";
 import img12 from "../../images/icono2.svg";
 
+
 function Home() {
+  // funcion hamburguesa
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const closeMenu = () => {
+    if (menuVisible) {
+      setMenuVisible(false);
+    }
+  };
+  // funcion galeria
+  const images = [img2, img3, img4, img5, img6, img7];
+  const [lightboxVisible, setLightboxVisible] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openLightbox = (image) => {
+    console.log("Abriendo lightbox con imagen:", image);
+    setSelectedImage(image);
+    setLightboxVisible(true);
+  };
+  
+  const closeLightbox = () => {
+    console.log("Cerrando lightbox");
+    setLightboxVisible(false);
+  };
+  
+
+  
+
+
+
   return (
-    <div className="bodyHome">
+    // -------hamburguesa
+    <div className={`bodyHome ${menuVisible ? 'menu-open' : ''}`}>
       <header className="header" id="inicio">
         <nav className="navbar navbar-dark bg-transparent" id="nav1">
           <div className="container-fluid">
-            <i className="bi bi-list hamburguesa" id="button-burger"></i>
+            <i className="bi bi-list hamburguesa" id="button-burger" onClick={toggleMenu}></i>
             <a href="/login" className="nav-link color d-sm-inline" id="option" >
-            Ingresar <i className="bi bi-box-arrow-in-right"></i>
+              Ingresar <i className="bi bi-box-arrow-in-right"></i>
             </a>
           </div>
         </nav>
         <div></div>
-        <nav className="menu_navegacion">
-          <a href="#inicio">Inicio</a>
-          <a href="#informacion">Información general</a>
-          <a href="#portafolio">Instalaciones</a>
-          <a href="#sobre_nosotros">Ambiente educativo</a>
-          <a href="#contacto">Contacto</a>
+        <nav className={`menu_navegacion ${menuVisible ? 'spread' : ''}`}>
+          <a href="#inicio" onClick={closeMenu}>Inicio</a>
+          <a href="#informacion" onClick={closeMenu}>Información general</a>
+          <a href="#portafolio" onClick={closeMenu}>Instalaciones</a>
+          <a href="#sobre_nosotros" onClick={closeMenu}>Ambiente educativo</a>
+          <a href="#contacto" onClick={closeMenu}>Contacto</a>
         </nav>
+        {/* ----- */}
         <div className="contenedor head">
-          <h1 className="titulo">Colegio Agustiniano norte</h1>
+          <h1 className="titulo">COLEGIO I.T.D. JUAN DEL CORRAL I.E.D.</h1>
           <p className="copy">
-            Lorem ipsum dolor sit amet consectetur adipisicing.
+            “La formación humana, científica y tecnológica para el desarrollo del ciudadano en el siglo XXI”
           </p>
         </div>
       </header>
-      
-        <div className="header" id="informacion">
-          <h2 className="subtitulo">Nuestra institución</h2>
-          <div className="contenedor_objetivos">
-            <img src={img1} alt="" />
-            <div className="puntos_objetivo">
-              <div className="objetivo">
-                <h3 className="n_objetivo">
-                  <span className="numero">1</span>objetivo
-                </h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-                  quos fugiat placeat labore a veniam quidem recusandae facere
-                  alias eum.
-                </p>
-              </div>
-              <div className="objetivo">
-                <h3 className="n_objetivo">
-                  <span className="numero">2</span>Alcance
-                </h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-                  quos fugiat placeat labore a veniam quidem recusandae facere
-                  repellat.
-                </p>
-              </div>
-              <div className="objetivo">
-                <h3 className="n_objetivo">
-                  <span className="numero">3</span>Delimitación
-                </h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-                  quos fugiat placeat labore a veniam quidem recusandae facere
-                  alias eum.
-                </p>
-              </div>
+
+      <div className="header" id="informacion">
+        <h2 className="subtitulo">Nuestra institución</h2>
+        <div className="contenedor_objetivos">
+          <img src={img1} alt="" />
+          <div className="puntos_objetivo">
+            <div className="objetivo">
+              <h3 className="n_objetivo">
+                <span className="numero">1</span>mision
+              </h3>
+              <p>
+                El Colegio Distrital Instituto Técnico Juan del Corral es una institución educativa distrital articulada con el SENA, que ofrece a niños, niñas y jóvenes una formación ciudadana integral a través del fortalecimiento de valores y capacidades para la ciudadanía y la convivencia y el desarrollo de programas académicos y técnicos, tales como Contabilización de procesos contables y financieros y Desarrollo gráfico de Proyectos de construcción, promoviendo su incorporación a actividades productivas que contribuyan al desarrollo sostenible, social, económico y tecnológico de la capital y del país.
+              </p>
             </div>
-          </div>
-          <br />
-        </div>
-        <div className="galeria" id="portafolio">
-          <div className="contenedor">
-            <h2 className="subtitulo">Instalaciones</h2>
-            <div className="contenedor_galeria">
-              <img src={img2} alt="img1" className="img1" />
-              <img src={img3} alt="img1" className="img1" />
-              <img src={img4} alt="img1" className="img1" />
-              <img src={img5} alt="img1" className="img1" />
-              <img src={img6} alt="img1" className="img1" />
-              <img src={img7} alt="img1" className="img1" />
+            <div className="objetivo">
+              <h3 className="n_objetivo">
+                <span className="numero">2</span>vision
+              </h3>
+              <p>
+                En el año 2026, el Colegio Distrital Instituto Técnico Juan del Corral será reconocido como líder en el ámbito distrital por aportar a la sociedad colombiana bachilleres técnicos con un excelente nivel académico, una amplia conciencia de responsabilidad social y formados en valores ciudadanos, que lideren procesos de emprendimiento en espacios de formación técnica-laboral para que mejoren su calidad de vida y contribuyan a la construcción de una sociedad más justa, equitativa y pací ca.
+              </p>
+            </div>
+            <div className="objetivo">
+              <h3 className="n_objetivo">
+                <span className="numero">3</span>Filosofía institucional
+              </h3>
+              <p>
+                La filosofía institucional del Colegio Distrital Instituto Técnico Juan del Corral se estructura en torno a dos ejes. Uno relacionado con la formación académica y técnica como elemento funda- mental que contribuye a la realización del proyecto de vida de los estudiantes, y otro que tiene que ver con el desarrollo humano y la formación integral del ciudadano que requiere la ciudad y el país para superar las brechas, las desigualdades, y sobre todo, para aportar a la convivencia pací ca en el posconflicto.
+              </p>
             </div>
           </div>
         </div>
-        <section className="imagen_trans">
-          <img src={img8} alt="" className="cerrar" />
-          <img src={img9} alt="" className="agregar_imagen" />
-        </section>
-        <section className="contenedor" id="sobre_nosotros">
-          <h2 className="subtitulo">Ambiente Estudiantil</h2>
-          <section className="equipo">
-            <div className="cont_equipo">
-              <img src={img10} alt="" />
-              <h3 className="n_equipo">Docentes</h3>
-            </div>
-            <div className="cont_equipo">
-              <img src={img11} alt="" />
-              <h3 className="n_equipo">Coordinadores</h3>
-            </div>
-            <div className="cont_equipo">
-              <img src={img12} alt="" />
-              <h3 className="n_equipo">Inscripciones</h3>
-            </div>
+        <br />
+      </div>
+      {/* _______galeria */}
+      <div className="galeria" id="portafolio">
+        <div className="contenedor">
+          <h2 className="subtitulo">Instalaciones</h2>
+          <div className="contenedor_galeria">
+            {images.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`img${index + 2}`}
+                className="img1"
+                onClick={() => openLightbox(image)}
+              />
+            ))}
+          </div>
+        </div>
+
+        {lightboxVisible && (
+          <section className="imagen_trans" onClick={closeLightbox}>
+            <img src={selectedImage} alt="" className="cerrar" />
           </section>
+        )}
+
+      </div>
+      {/* ______ */}
+      <section className="contenedor" id="sobre_nosotros">
+        <h2 className="subtitulo">Ambiente Estudiantil</h2>
+        <section className="equipo">
+          <div className="cont_equipo">
+            <img src={img10} alt="" />
+            <h3 className="n_equipo">Docentes</h3>
+          </div>
+          <div className="cont_equipo">
+            <img src={img11} alt="" />
+            <h3 className="n_equipo">Coordinadores</h3>
+          </div>
+          <div className="cont_equipo">
+            <img src={img12} alt="" />
+            <h3 className="n_equipo">Inscripciones</h3>
+          </div>
         </section>
-     
+      </section>
+
 
       <Footer></Footer>
     </div>
